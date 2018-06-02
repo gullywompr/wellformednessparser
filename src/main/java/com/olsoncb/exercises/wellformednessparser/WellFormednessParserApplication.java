50103f2b6210ea5279d8f11ed0c4bc6be004ed78
Curtis Olson <colson@optimo-it.com>
2018-06-02 18:40:22 -0400

    Initial commit

---
+++ src/main/java/com/olsoncb/exercises/wellformednessparser/WellFormednessParserApplication.java
@@ -0,0 +1,9 @@
+package com.olsoncb.exercises.wellformednessparser;
+
+public class WellFormednessParserApplication {
+
+	public static void main(String[] args) {
+
+
+	}
+}
---
+++ src/test/java/com/olsoncb/exercises/wellformednessparser/WellformednessparserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.olsoncb.exercises.wellformednessparser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WellformednessparserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
