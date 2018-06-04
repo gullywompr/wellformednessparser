8b9b71aef6127f219280c3852736ca2ba60dc9ab
Curtis Olson <colson@optimo-it.com>
2018-06-04 14:30:21 -0400

    Cleanup

--- src/main/java/com/olsoncb/exercises/wellformednessparser/ParserState.java
+++ src/main/java/com/olsoncb/exercises/wellformednessparser/ParserState.java
@@ -2,9 +2,9 @@ package com.olsoncb.exercises.wellformednessparser;
 
 public enum ParserState {
   OPEN_TAG_INITIATED,
+  OPEN_TAG_NAMED,
+  OPEN_TAG_AUTOCLOSE_INITIATED,
   OPEN_TAG_TERMINATED,
   CLOSE_TAG_INITIATED,
-  OPEN_TAG_AUTOCLOSE_INITIATED,
-  OPEN_TAG_NAMED,
   CLOSE_TAG_TERMINATED
 }
